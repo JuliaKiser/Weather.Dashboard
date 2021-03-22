199284176a4cdd93927d64171dce479df139874a
J_Kiser <jdkiser228@gmail.com>
2021-03-21 21:29:31 -0400

    working on javascript

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+var citysearched = "charlotte"
+
+$.ajax ({
+    url: "api.openweathermap.org/data/2.5/weather?q=" + citysearched + "&appid=2c43a84955b43240c8d4fb64e9a027c5",
+    method: "GET"
+})
+.then(function(response) {
+    console.log(response)
+    if(!searches.include(response.name)) {
+        searches.push(response.name);
+    }
+    localStorage.setItem("searches",JSON.stringify(searches));
+    $("#searchcity").val("");
+    renderHistory();
+});
+
+var searchedCity = $("#selected");
+console.log(response)
+searchedCity.append($(".citysearched").text(response.name + " (" + moment().format('L') + ")"));
+searchedCity.append($(".temperature").text("Current Temperature (F): " + response.main.temp.toFixed(2)));
+
+
